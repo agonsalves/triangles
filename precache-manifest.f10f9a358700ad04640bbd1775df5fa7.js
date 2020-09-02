642c11fac5b9b9a98b8d140b955a2762ffb6649a
Andrew Gonsalves <agonsalves@greatjakes.com>
2020-09-02 09:04:40 -0400

    Updates

---
+++ precache-manifest.f10f9a358700ad04640bbd1775df5fa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eed6852c55077452697de9bdda1c8e0",
+    "url": "/triangles/index.html"
+  },
+  {
+    "revision": "cf6627fafc3c15ba0c52",
+    "url": "/triangles/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b4670e94b2aa823f9782",
+    "url": "/triangles/static/js/2.f473606d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/triangles/static/js/2.f473606d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6627fafc3c15ba0c52",
+    "url": "/triangles/static/js/main.bd560e2b.chunk.js"
+  },
+  {
+    "revision": "ab32d0cc88c0010e048e",
+    "url": "/triangles/static/js/runtime-main.f07fb669.js"
+  }
+]);
